512113861a8b4ea34f968c60c7aea44659d7521f
Ghazali <mghazk@yahoo.com>
2020-12-15 11:38:05 +0800

    Add Function index in trainingcontroller

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Training;
 
 class TrainingController extends Controller
 {
-    //
+    public function index()
+    {
+        $trainings = Training::all();
+
+        // resources/views/trainings/index.blade.php
+        return view('trainings.index');
+    }
 }
