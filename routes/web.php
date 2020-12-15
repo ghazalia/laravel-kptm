0dbb4a986bda3c3fe1ed42009f697a6a48144c08
Ghazali <mghazk@yahoo.com>
2020-12-15 11:38:39 +0800

    Add route trainings

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/trainings', [App\Http\Controllers\TrainingController::class, 'index']);
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
